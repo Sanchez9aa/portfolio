c846a70bc3eeee19a682b92e53f01da4b12256ff
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-13 17:45:15 +0100

    Update vData.js

--- client/src/vData.js
+++ client/src/vData.js
@@ -19,6 +19,61 @@ const vData = [
     arquitectura: "No tiene",
     heroku: "https://apirestcountry.herokuapp.com/",
     github: "https://github.com/Sanchez9aa/ApiRestCountries"
+  },
+  {
+    project: "mechWar",
+    header: "Projecto mech War",
+    link: "",
+    summary: "Este proyecto es un full backend con Nodejs. Se envia ciertos protocolos para atacar, dependiendo de los protocolos se atacará a x coordenada.",
+    frontend: "No tiene",
+    backend: "Node.js, express",
+    arquitectura: "MVC",
+    heroku: "https://mechswar.herokuapp.com/",
+    github: "https://github.com/Sanchez9aa/MechWar"
+  },
+  {
+    project: "martianRobots",
+    header: "Projecto Martian Robots",
+    link: "",
+    summary: "Este proyecto es un full backend con Nodejs. Se envian x robots con x sequencias en una superficie dada (supuestamente cuadrada y en marte), tiene que devolvernos la posicion en las que se quedan los robots y si se han perdido, si se han perdido dejan como un olor para que el robot que venga por atrás no se pierda.",
+    frontend: "No tiene",
+    backend: "Node.js, express, mongodb (mongoose), jest",
+    arquitectura: "MVC",
+    heroku: "https://martiansrobots.herokuapp.com/",
+    github: "https://github.com/Sanchez9aa/MartianRobots"
+  },
+  {
+    project: "phoneCatalog",
+    header: "Projecto Catálogo de móviles",
+    link: "",
+    summary: "Proyecto Full Stack con MERN y configuradas aplicaciones CRUD para teléfonos móviles, tiene modo oscuro.",
+    frontend: "React, hooks, axios, react-router-dom, react-feather, css3",
+    backend: "Node.js, express, mongodb (mongoose), cors",
+    arquitectura: "MVC",
+    heroku: "https://phonescatalog.herokuapp.com/",
+    github: "https://github.com/Sanchez9aa/PhoneCatalog"
+  },
+  {
+    project: "calculatorApp",
+    header: "Projecto Calculadora",
+    link: "",
+    summary: "Calculadora con 3 themas diferentes en la cual podemos hacer las operaciones más básicas",
+    frontend: "React, styled-components, hooks, jest",
+    backend: "No tiene",
+    arquitectura: "No tiene",
+    heroku: "https://fmcalculatorapp.herokuapp.com/",
+    github: "https://github.com/Sanchez9aa/calculatorapp"
+  },
+  {
+    project: "calculatorInditexChallenge",
+    header: "Projecto Challenge Calculadora de inditex ",
+    link: "",
+    summary: "Tomo prestado los estilos de mi proyecto anterior, pero este tiene la operacionalidad con redux en vez de con hooks, también se le incluye el modo arrays, explicado en el readme de github",
+    frontend: "React, styled-components, redux, jest",
+    backend: "No tiene",
+    arquitectura: "No tiene",
+    heroku: "https://calculatorinditexchallenge.herokuapp.com/",
+    github: "https://github.com/Sanchez9aa/CalculatorInditexChallenge"
   }
 ]
 
